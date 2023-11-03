d9f369a726ecda10112e0e14e4210c17b8cec1a8
Cym3ra <112971484+Cym3ra@users.noreply.github.com>
2023-11-03 10:49:09 +0100

    AI follow target logic started

--- Source/KillDrome/KillDrome.Build.cs
+++ Source/KillDrome/KillDrome.Build.cs
@@ -8,9 +8,9 @@ public class KillDrome : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "NavigationSystem", "Niagara",   "AIModule", "GameplayTasks" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "NavigationSystem", "Niagara",   "AIModule"});
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
---
+++ Source/KillDrome/Private/AI/BTService_FindNearestBike.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/BTService_FindNearestBike.h"
+
+#include "AIController.h"
+#include "Kismet/GameplayStatics.h"
+
+void UBTService_FindNearestBike::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	APawn* OwningPawn = AIOwner->GetPawn();
+
+	const FName TargetTag = OwningPawn->ActorHasTag(FName("Player")) ? FName("Enemy") :  FName("Player");
+
+	TArray<AActor*> ActorsWithTag;
+	UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTag);
+}
